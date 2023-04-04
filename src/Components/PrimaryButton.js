c44f85b64706b0aa6cb77dcd637c0683adc57f8d
IrenSiren <71807032+IrenSiren@users.noreply.github.com>
2023-04-04 17:41:04 +0000

    Обновила инпуты и немного поменяла хэдер

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -7,16 +7,17 @@ function PrimaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth; 
     const Button = styled.div`
-        background: #4B759E;
-        border: 1px solid #152842;
+        background: #3B82F6;
+        border: 1px solid #3B82F6;        
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
         color: white;
+        border-radius: 24px;
         &:hover {
-            background: linear-gradient(180deg, #4D9FF0 0%, #36699D 100%);
+            background: #5894F4;
         }
         &:active {
-            background: #152842;
+            background: rgba(248,250,252);
         }
     `;
     return (
