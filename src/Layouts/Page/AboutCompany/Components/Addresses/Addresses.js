c1ebfdbcd720685ff26058f6c36035df725dac69
IrenSiren <71807032+IrenSiren@users.noreply.github.com>
2023-04-10 18:00:18 +0000

    Поменяла марджыны и паддинги

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,18 +3,19 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4 border'>
-            <div className='d-flex'flex-column p-4 gap-4>
+        <div className='d-inline-flex flex-column gap-4 mx-5 p-4 border'>
+            
                 <div className='title'>Адреса заведений</div>
                 <div className='d-inline-flex flex-column gap-2'>
                     <div className='d-flex flex-column gap-2'>
                         <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                         <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                     </div>
-                    <SecondaryButton>Добавить филиал</SecondaryButton>
                 </div>
-            </div>
+            
+                    <SecondaryButton>Добавить филиал</SecondaryButton>
         </div>
+        
     );
 }
 
